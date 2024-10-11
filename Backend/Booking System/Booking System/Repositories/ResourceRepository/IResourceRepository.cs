7e160fd40c60f233ff58c04c3cfe2e00208737cc
Samaan Kamal <samaankamal72@gmail.com>
2024-10-11 20:28:02 +0300

    added CRUD methods for Resource interface and class impelementation

--- Backend/Booking System/Booking System/Repositories/ResourceRepository/IResourceRepository.cs
+++ Backend/Booking System/Booking System/Repositories/ResourceRepository/IResourceRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Booking_System.Repositories
+﻿using Booking_System.Model;
+
+namespace Booking_System.Repositories.ResourceRepository
 {
     public interface IResourceRepository
     {
+        Task<IEnumerable<Resource>> GetAllResourcesAsync();
+        Task<Resource> GetResourceAsync(int id);
+        Task<Resource> AddResourceAsync(Resource resource);
+        Task DeleteResourceAsync(int id);
+        Task UpdateResourceAsync(Resource resource);
     }
 }
--- Backend/Booking System/Booking System/Repositories/ResourceRepository/ResourceRepositoryImp.cs
+++ Backend/Booking System/Booking System/Repositories/ResourceRepository/ResourceRepositoryImp.cs
@@ -1,6 +1,48 @@
-﻿namespace Booking_System.Repositories.ResourceRepository
+﻿using Booking_System.Data;
+using Booking_System.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booking_System.Repositories.ResourceRepository
 {
-    public class ResourceRepositoryImp
+    public class ResourceRepositoryImp : IResourceRepository
     {
+        private readonly BookingSystemContext _context;
+
+        public ResourceRepositoryImp(BookingSystemContext bookingSystemContext) {
+            _context = bookingSystemContext;
+        }
+
+        public async Task<Resource> AddResourceAsync(Resource resource)
+        {
+            await _context.Resources.AddAsync(resource);
+            await _context.SaveChangesAsync();
+            return resource;
+        }
+
+        public async Task DeleteResourceAsync(int id)
+        {
+            Resource resource = await GetResourceAsync(id);
+            if (resource != null)
+            {
+                _context.Resources.Remove(resource);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Resource>> GetAllResourcesAsync()
+        {
+            return await _context.Resources.ToListAsync();
+        }
+
+        public async Task<Resource> GetResourceAsync(int id)
+        {
+            return await _context.Resources.FindAsync(id);
+        }
+
+        public async Task UpdateResourceAsync(Resource resource)
+        {
+            _context.Resources.Update(resource);
+            await _context.SaveChangesAsync();
+        }
     }
 }
