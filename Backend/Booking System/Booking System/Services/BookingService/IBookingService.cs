efab6952415a888750e502689acc42bcca870811
Samaan Kamal <samaankamal72@gmail.com>
2024-10-11 19:50:25 +0300

    changed the request object of the service to use DTO object

--- Backend/Booking System/Booking System/Services/BookingService/IBookingService.cs
+++ Backend/Booking System/Booking System/Services/BookingService/IBookingService.cs
@@ -1,6 +1,11 @@
-﻿namespace Booking_System.Services.BookingService
+﻿using Booking_System.DTO;
+using Booking_System.Model;
+
+namespace Booking_System.Services.BookingService
 {
     public interface IBookingService
     {
+        Task<bool> IsBookingAvailableAsync(BookingRequestDTO bookingRequest);
+        Task<Booking> CreateBookingAsync(BookingRequestDTO bookingRequest);
     }
 }
