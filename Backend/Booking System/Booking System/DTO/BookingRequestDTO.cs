7615f7e428415c77591bae6b39dcf8192d151e9d
Samaan Kamal <samaankamal72@gmail.com>
2024-10-11 19:54:47 +0300

    Pushed DTO Code into github

--- Backend/Booking System/Booking System/DTO/BookingRequestDTO.cs
+++ Backend/Booking System/Booking System/DTO/BookingRequestDTO.cs
@@ -2,5 +2,9 @@
 {
     public class BookingRequestDTO
     {
+        public int ResourceId { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public int Quantity { get; set; }
     }
 }
--- Backend/Booking System/Booking System/DTO/ResourceRequestDTO.cs
+++ Backend/Booking System/Booking System/DTO/ResourceRequestDTO.cs
@@ -2,5 +2,8 @@
 {
     public class ResourceRequestDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TotalQuantity { get; set; }
     }
 }
