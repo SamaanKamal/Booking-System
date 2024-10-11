5dd621396ef46a9ad2d42785c9f50b7e56a1a402
Samaan Kamal <samaankamal72@gmail.com>
2024-10-11 20:28:33 +0300

    pused code of repositories interface and impelementation

--- Backend/Booking System/Booking System/Repositories/BookingRepository/BookingRepositoryImp.cs
+++ Backend/Booking System/Booking System/Repositories/BookingRepository/BookingRepositoryImp.cs
@@ -1,6 +1,26 @@
-﻿namespace Booking_System.Repositories.BookingRepository
+﻿using Booking_System.Data;
+using Booking_System.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booking_System.Repositories.BookingRepository
 {
-    public class BookingRepositoryImp
+    public class BookingRepositoryImp : IBookingRepository
     {
+        private readonly BookingSystemContext _context;
+
+        public BookingRepositoryImp(BookingSystemContext bookingSystemContext) {
+            _context = bookingSystemContext;
+        }
+        public async Task<Booking> AddBookingAsync(Booking booking)
+        {
+           await _context.Bookings.AddAsync(booking);
+           await _context.SaveChangesAsync();
+           return booking;
+        }
+
+        public async Task<IEnumerable<Booking>> GetBookingsByResourceAndDateRangeAsync(int resourceId, DateTime startDate, DateTime endDate)
+        {
+            return await _context.Bookings.Where(booking => booking.ResouceId == resourceId && booking.StartTime < endDate && booking.EndTime > startDate).ToListAsync();
+        }
     }
 }
--- Backend/Booking System/Booking System/Repositories/BookingRepository/IBookingRepository.cs
+++ Backend/Booking System/Booking System/Repositories/BookingRepository/IBookingRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace Booking_System.Repositories.BookingRepository
+﻿using Booking_System.Model;
+
+namespace Booking_System.Repositories.BookingRepository
 {
     public interface IBookingRepository
     {
+        Task<IEnumerable<Booking>> GetBookingsByResourceAndDateRangeAsync(int resourceId, DateTime startDate, DateTime endDate);
+        Task<Booking> AddBookingAsync(Booking booking);
     }
 }
