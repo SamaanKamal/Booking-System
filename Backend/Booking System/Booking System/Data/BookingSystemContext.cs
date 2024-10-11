9b3e4285a371f3adfb35090bd13f297e32b5aa4c
Samaan Kamal <samaankamal72@gmail.com>
2024-10-11 19:55:33 +0300

    pushed Booking Context into github

--- Backend/Booking System/Booking System/Data/BookingSystemContext.cs
+++ Backend/Booking System/Booking System/Data/BookingSystemContext.cs
@@ -1,6 +1,15 @@
-﻿namespace Booking_System.Data
+﻿using Booking_System.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booking_System.Data
 {
-    public class BookingSystemContext
+    public class BookingSystemContext:DbContext
     {
+        public BookingSystemContext(DbContextOptions<BookingSystemContext> options) : base(options) {
+
+        }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
