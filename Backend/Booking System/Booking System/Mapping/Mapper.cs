ca491613a16c9a8f1f6aa62490df0ebef2767f13
Samaan Kamal <samaankamal72@gmail.com>
2024-10-11 22:50:17 +0300

    added Mapper Class Between Model and DTO

---
+++ Backend/Booking System/Booking System/Mapping/Mapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Booking_System.DTO;
+using Booking_System.Model;
+
+namespace Booking_System.Mapping
+{
+    public class Mapper:Profile
+    {
+        public Mapper() {
+            CreateMap<Resource, ResourceRequestDTO>().ReverseMap();
+            CreateMap<Booking, BookingRequestDTO>().ReverseMap();
+        }
+    }
+}
