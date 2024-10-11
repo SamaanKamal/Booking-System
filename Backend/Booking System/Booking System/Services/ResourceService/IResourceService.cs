9a550fbd41b6c63f8d932b4cd459c2a7c1c83641
Samaan Kamal <samaankamal72@gmail.com>
2024-10-11 22:48:52 +0300

    added Interface for Resource CRUD operations

---
+++ Backend/Booking System/Booking System/Services/ResourceService/IResourceService.cs
@@ -0,0 +1,14 @@
+﻿using Booking_System.DTO;
+using Booking_System.Model;
+
+namespace Booking_System.Services.ResourceService
+{
+    public interface IResourceService
+    {
+        Task<IEnumerable<Resource>> GetAllResourcesAsync();
+        Task<Resource> GetResourceByIdAsync(int id);
+        Task<Resource> AddResourceAsync(ResourceRequestDTO resourceRequest);
+        Task UpdateResourceAsync(int id,ResourceRequestDTO resourceRequest);
+        Task DeleteResourceAsync(int id);
+    }
+}
