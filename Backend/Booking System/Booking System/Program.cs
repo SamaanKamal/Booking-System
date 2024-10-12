a6d15b07691ef6c3170c5e393c0de7c72f0c331b
Samaan Kamal <samaankamal72@gmail.com>
2024-10-12 19:31:00 +0300

    registered services and repositories and mapper to use DI pattern

--- Backend/Booking System/Booking System/Program.cs
+++ Backend/Booking System/Booking System/Program.cs
@@ -1,3 +1,9 @@
+using Booking_System.Repositories.BookingRepository;
+using Booking_System.Repositories.ResourceRepository;
+using Booking_System.Services.BookingService;
+using Booking_System.Services.ResourceService;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+// Register services
+builder.Services.AddScoped<IResourceRepository, ResourceRepositoryImp>();
+builder.Services.AddScoped<IResourceService,ResourceService>();
+builder.Services.AddScoped<IBookingRepository, BookingRepositoryImp>();
+builder.Services.AddScoped<IBookingService, BookingService>();
+
 
 var app = builder.Build();
 
