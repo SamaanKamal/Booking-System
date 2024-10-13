587b0dd9e499ca97573b81903332c436807eb0ec
Samaan Kamal <samaankamal72@gmail.com>
2024-10-13 03:54:36 +0300

    added loading spinner to list of Resources while fetching

--- Frontend/Booking-System/src/components/Resources/ResourceList.jsx
+++ Frontend/Booking-System/src/components/Resources/ResourceList.jsx
@@ -2,18 +2,22 @@ import React, { useState, useEffect } from "react";
 import { FetchResources } from "../../services/FetchResources";
 import ResourceTable from "./ResourceTable";
 import Booking from "../Booking/Booking";
+import LoadingSpinner from "../../shared/loading-spinner";
 
 const ResourceList = (props) => {
   const [resources, setResources] = useState([]);
   const [selectedResource, setSelectedResource] = useState(null);
-
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const loadResources = async () => {
       try {
         const data = await FetchResources();
         setResources(data);
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching resources:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -29,7 +33,11 @@ const ResourceList = (props) => {
   };
   return (
     <div>
-      <ResourceTable resources={resources} onBookingClick={handleBooking} />
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <ResourceTable resources={resources} onBookingClick={handleBooking} />
+      )}
       {selectedResource && (
         <Booking resource={selectedResource} onClose={handleDialogClose} />
       )}
