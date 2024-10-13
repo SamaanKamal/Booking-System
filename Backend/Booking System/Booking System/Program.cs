9a74e0c856acb7e59cb1c2ea4120fb6ba585a11c
Samaan Kamal <samaankamal72@gmail.com>
2024-10-13 18:59:15 +0300

    allowed cors from other domains

--- Backend/Booking System/Booking System/Program.cs
+++ Backend/Booking System/Booking System/Program.cs
@@ -1,7 +1,10 @@
+using Booking_System.Data;
 using Booking_System.Repositories.BookingRepository;
 using Booking_System.Repositories.ResourceRepository;
 using Booking_System.Services.BookingService;
 using Booking_System.Services.ResourceService;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +15,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BookingSystemContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddAutoMapper(typeof(Program));
 
@@ -20,6 +25,13 @@ builder.Services.AddScoped<IResourceRepository, ResourceRepositoryImp>();
 builder.Services.AddScoped<IResourceService,ResourceService>();
 builder.Services.AddScoped<IBookingRepository, BookingRepositoryImp>();
 builder.Services.AddScoped<IBookingService, BookingService>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
 
 
 var app = builder.Build();
@@ -31,10 +43,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAll");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
